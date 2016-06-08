2d104db36487cedc42cedb789311f34110a78239
WumboGames <mywumbogames@gmail.com>
2016-06-07 20:37:53 -0500

    angle works!

--- src/game/Simulation.java
+++ src/game/Simulation.java
@@ -8,31 +8,14 @@ import robot.Window;
 
 public class Simulation {
 	private float[][] paper;
-	private float[][] edges;
+	private Edge[][] edges;
 	private float cutoff=.07f;
 	public Simulation(Color[][] paper) {
 		this.paper=Window.getNonRedLuminanceOfImage(paper);
-		edges=Edge.toFloatArray(EdgeDetection.sobelEdgeDetection(this.paper));
-		double max=0;
-		for (float[] a:edges)
-			for (float b:a) {
-				max=Math.max(max, b);
-			}
-		System.out.println("\t"+max);
+		edges=EdgeDetection.sobelEdgeDetection(this.paper);
 	}
 	
 	public Color[][] render() {
-		Color[][] toReturn=new Color[edges.length][edges[0].length];
-		for (int x=0; x<toReturn.length; x++) {
-			for (int y=0; y<toReturn[x].length; y++) {
-				if (edges[x][y]>cutoff) {
-					toReturn[x][y]=Color.blue;
-				}
-				else {
-					toReturn[x][y]=new Color(0, 0, 0, 0);
-				}
-			}
-		}
-		return toReturn;
+		return Edge.toColorArray(edges, cutoff);
 	}
 }
--- src/processing/Edge.java
+++ src/processing/Edge.java
@@ -1,5 +1,7 @@
 package processing;
 
+import java.awt.Color;
+
 public class Edge {
 	public float intensity;
 	public float angle;
@@ -14,6 +16,27 @@ public class Edge {
 		for (int x=0; x<toReturn.length; x++) {
 			for (int y=0; y<toReturn[x].length; y++) {
 				toReturn[x][y]=edges[x][y].intensity;
+			}
+		}
+		return toReturn;
+	}
+	
+	public static Color[][] toColorArray(Edge[][] edges, float cutoff) {
+		Color[][] toReturn=new Color[edges.length][edges[0].length];
+		for (int x=0; x<toReturn.length; x++) {
+			for (int y=0; y<toReturn[x].length; y++) {
+				if (edges[x][y].intensity>cutoff) {
+					float angle=edges[x][y].angle;
+					double blue=Math.cos(angle);
+					double red=Math.sin(angle);
+					if (red>0)
+						toReturn[x][y]=new Color((float)red, 0.0f, (float)blue, 1.0f);
+					else 
+						toReturn[x][y]=new Color(0f, -(float)red, (float)blue, 1.0f);
+				}
+				else {
+					toReturn[x][y]=new Color(0, 0, 0, 0);
+				}
 			}
 		}
 		return toReturn;
