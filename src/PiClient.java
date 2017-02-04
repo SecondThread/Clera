2e1e30bec3df058435bd419be9fea59ee9090363
YoshiPWNs <hiddendragon66@gmail.com>
2017-02-04 15:14:51 -0600

    Revert "Added the method to find shooterDegreesToTurn. However, it doesn't go in network tables"
    This reverts commit f42937f5370e3d37a63b23c6982389605c5026da.

--- src/PiClient.java
+++ src/PiClient.java
@@ -9,7 +9,6 @@ import com.github.sarxos.webcam.Webcam;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.tables.ITable;
 import edu.wpi.first.wpilibj.tables.ITableListener;
-import processing.FindOnePtTop;
 import processing.ImageProcessor;
 import processing.ImageSearchingThread;
 import processing.PegVisionUtils;
@@ -19,9 +18,7 @@ import robot.Window;
 public class PiClient {
 
 	private static Webcam webcam;
-	private static Webcam shooterWebcam;
 	private static ArrayList<Point> foundPoints;
-	private static Point shooterPoint;
 	private static double distance;
 
 	public static void main(String[] a) {
@@ -34,79 +31,62 @@ public class PiClient {
 				System.out.println("Value changed, key: " + key);
 				if (key.equals("processVision")) {
 					if (table.getBoolean(key, false)) {
-						table.putNumber("pegDegreesToTurn", getDegreesToTurn(0));
+						table.putNumber("degreesToTurn", getDegreesToTurn(0));
 						table.putNumber("distanceToMove", distance);
 						table.putBoolean("processVision", false);
-						table.putNumber("distanceToPegInches",
-								PegVisionUtils.calcDistance(PegVisionUtils.generateNewPoints(foundPoints)));
+						table.putNumber("distanceToPegInches", PegVisionUtils.calcDistance(PegVisionUtils.generateNewPoints(foundPoints)));
 					}
 				}
 			}
 		});
 	}
-
+	
 	public static void init() {
-		webcam = Webcam.getDefault();
+		webcam=Webcam.getDefault();
 		webcam.setViewSize(new Dimension(320, 240));
 		webcam.open();
 	}
 
 	public static float getDegreesToTurn(int runCounter) {
-		float[][] luminance = null;
-		ArrayList<Point> bestPoints = null;
-		System.out.println("start: " + System.currentTimeMillis());
-		BufferedImage image = getImageFromWebcam(webcam);
-		Color[][] asColors = new Color[image.getWidth()][image.getHeight()];
+		float[][] luminance=null;
+		ArrayList<Point> bestPoints=null;
+		System.out.println("start: "+System.currentTimeMillis());
+		BufferedImage image=getImageFromWebcam(webcam);
+		Color[][] asColors=new Color[image.getWidth()][image.getHeight()];
 
-		for (int x = 0; x < asColors.length; x++) {
-			for (int y = 0; y < asColors[x].length; y++) {
-				asColors[x][y] = new Color(image.getRGB(x, y));
+		for (int x=0; x<asColors.length; x++) {
+			for (int y=0; y<asColors[x].length; y++) {
+				asColors[x][y]=new Color(image.getRGB(x, y));
 			}
 		}
-		luminance = ImageProcessor.luminance(asColors, -.2f, 1.0f, -.2f);
+		luminance=ImageProcessor.luminance(asColors, -.2f, 1.0f, -.2f);
 		ImageProcessor.normalize(luminance);
 		ImageProcessor.applyExponentialCurve(luminance, 3);
 
-		bestPoints = new ArrayList<Point>();
-		bestPoints = runThreads(luminance);
-		// null if points are wrong
-		if (bestPoints == null) {
-			if (runCounter >= 3) {
+		bestPoints=new ArrayList<Point>();
+		bestPoints=runThreads(luminance);
+		//null if points are wrong
+		if (bestPoints==null) {
+			if (runCounter>=3) {
 				return 0;
 			}
-			// try it again if we got things wrong
-			return getDegreesToTurn(runCounter + 1);
+			//try it again if we got things wrong
+			return getDegreesToTurn(runCounter+1);
 		}
-		System.out.println("done: " + System.currentTimeMillis());
+		System.out.println("done: "+System.currentTimeMillis());
 		Window.saveImage(luminance, bestPoints, "result.png");
 		foundPoints = bestPoints;
-		Point peg = PegVisionUtils.findPeg(bestPoints);
-		// length of dumbo, 50, width of image, best points
-		distance = PegVisionUtils.calcDistance(bestPoints);
+		Point peg=PegVisionUtils.findPeg(bestPoints);
+		//length of dumbo, 50, width of image, best points
+		distance=PegVisionUtils.calcDistance(bestPoints);
 		return TurnAngle.getTurnAngle(peg);
 	}
 
-	public static float getShooterDegreesToTurn(int runCounter, BufferedImage shooterImage) {
-		System.out.println("start: " + System.currentTimeMillis());
-		Point shooter = FindOnePtTop.findTopPoint(shooterImage);
-		shooterPoint = shooter;
-		// null if points are wrong
-		if (shooter.equals(new Point(0, 0))) {
-			if (runCounter >= 3) {
-				return 0;
-			}
-			// try it again if we got things wrong
-			return getShooterDegreesToTurn(runCounter + 1, shooterImage);
-		}
-		System.out.println("done: " + System.currentTimeMillis());
-		return TurnAngle.getTurnAngle(shooter);
-	}
-
 	private static ArrayList<Point> runThreads(float[][] image) {
-		ImageSearchingThread topLeft = new ImageSearchingThread(topLeftTemplate, image);
-		ImageSearchingThread topRight = new ImageSearchingThread(topRightTemplate, image);
-		ImageSearchingThread bottomLeft = new ImageSearchingThread(bottomLeftTemplate, image);
-		ImageSearchingThread bottomRight = new ImageSearchingThread(bottomRightTemplate, image);
+		ImageSearchingThread topLeft=new ImageSearchingThread(topLeftTemplate, image);
+		ImageSearchingThread topRight=new ImageSearchingThread(topRightTemplate, image);
+		ImageSearchingThread bottomLeft=new ImageSearchingThread(bottomLeftTemplate, image);
+		ImageSearchingThread bottomRight=new ImageSearchingThread(bottomRightTemplate, image);
 
 		new Thread(topRight).start();
 		new Thread(bottomLeft).start();
@@ -116,7 +96,7 @@ public class PiClient {
 		// bottomRight.run();
 		topLeft.run();
 
-		while (!topLeft.isDone() || !topRight.isDone() || !bottomLeft.isDone() || !bottomRight.isDone()) {
+		while (!topLeft.isDone()||!topRight.isDone()||!bottomLeft.isDone()||!bottomRight.isDone()) {
 			try {
 				Thread.sleep(5);
 			} catch (InterruptedException e) {
@@ -124,14 +104,14 @@ public class PiClient {
 			}
 		}
 
-		ArrayList<Point> toReturn = new ArrayList<Point>();
+		ArrayList<Point> toReturn=new ArrayList<Point>();
 		toReturn.addAll(topLeft.bestPoints);
 		toReturn.addAll(topRight.bestPoints);
 		toReturn.addAll(bottomLeft.bestPoints);
 		toReturn.addAll(bottomRight.bestPoints);
 
-		// null if the points are wrong
-		toReturn = PegVisionUtils.generateNewPoints(toReturn);
+		//null if the points are wrong
+		toReturn=PegVisionUtils.generateNewPoints(toReturn);
 
 		return toReturn;
 
@@ -141,16 +121,16 @@ public class PiClient {
 		return webcam.getImage();
 	}
 
-	private static float[][] topLeftTemplate = { { 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0, },
-			{ 0, 0, 0, 1, 1, 1, }, { 0, 0, 0, 1, 1, 1, }, { 0, 0, 0, 1, 1, 1, }, };
+	private static float[][] topLeftTemplate= {{0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0,},
+			{0, 0, 0, 1, 1, 1,}, {0, 0, 0, 1, 1, 1,}, {0, 0, 0, 1, 1, 1,},};
 
-	private static float[][] topRightTemplate = { { 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0 },
-			{ 1, 1, 1, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 }, };
+	private static float[][] topRightTemplate= {{0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0},
+			{1, 1, 1, 0, 0, 0}, {1, 1, 1, 0, 0, 0}, {1, 1, 1, 0, 0, 0},};
 
-	private static float[][] bottomLeftTemplate = { { 0, 0, 0, 1, 1, 1, }, { 0, 0, 0, 1, 1, 1, }, { 0, 0, 0, 1, 1, 1, },
-			{ 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0, }, };
+	private static float[][] bottomLeftTemplate= {{0, 0, 0, 1, 1, 1,}, {0, 0, 0, 1, 1, 1,}, {0, 0, 0, 1, 1, 1,},
+			{0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0,},};
 
-	private static float[][] bottomRightTemplate = { { 1, 1, 1, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, };
+	private static float[][] bottomRightTemplate= {{1, 1, 1, 0, 0, 0}, {1, 1, 1, 0, 0, 0}, {1, 1, 1, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0},};
 
 }
--- src/processing/GreenImageProcesser.java
+++ src/processing/GreenImageProcesser.java
@@ -1,11 +1,11 @@
 package processing;
-
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 /**
- * This class has the process method that process the image so that makes
- * OUTSTANDING green color pixel into a 1 and everything else into a 0
+ * This class has the process method that process the image 
+ * so that makes OUTSTANDING green color pixel into a 1
+ * and everything else into a 0
  * 
  * @author jialuo
  * 
@@ -15,123 +15,121 @@ public class GreenImageProcesser {
 	private float blueIndex;
 	private float greenIndex;
 	private double upperBound;
-
 	/**
 	 * Construction method
 	 */
-	public GreenImageProcesser() {
-		redIndex = -.45f;
-		greenIndex = 1.0f;
-		blueIndex = -.45f;
-		upperBound = 0.6;
+	public GreenImageProcesser(){
+		redIndex =  -.45f;
+		greenIndex= 1.0f;
+		blueIndex=  -.45f;
+		upperBound= 0.6;
 	}
-
 	/**
 	 * Construction method
-	 * 
-	 * @param redIndex
-	 *            : multiplier of red color value, domain from 0f to -1f
-	 *            recommended input: -0.2f to -0.4f for bright camera -0.4f to
-	 *            -0.7f for dim camera -0.7f to -0.9f for very dim camera
-	 * @param greenIndex
-	 *            : multiplier of green color value, domain from 0f to 1f
-	 *            recommended input: 1f
-	 * @param blueIndex
-	 *            : multiplier of red color value, domain from 0f to -1f
-	 *            recommended input: same as red
-	 * @param upperBound
-	 *            : remove all point that values below this bound recommended
-	 *            input: for input G/R of -0.2 to -0.4f : 0.7 to 0.9 for input
-	 *            G/R of -0.4f to -0.7f : 0.4 to 0.7 for input G/R of -0.7f to
-	 *            -0.9f : 0.1 to 0.4
+	 * @param redIndex 		: multiplier of red color value, domain from 0f to -1f 
+	 * 						  recommended input: -0.2f to -0.4f for bright camera
+	 * 											 -0.4f to -0.7f for dim camera
+	 * 											 -0.7f to -0.9f for very dim camera 
+	 * @param greenIndex	: multiplier of green color value, domain from 0f to 1f
+	 * 						  recommended input: 1f
+	 * @param blueIndex		: multiplier of red color value, domain from 0f to -1f 
+	 * 						  recommended input: same as red 
+	 * @param upperBound	: remove all point that values below this bound
+	 * 						  recommended input: for input G/R of -0.2 to -0.4f  : 0.7 to 0.9
+	 * 											 for input G/R of -0.4f to -0.7f : 0.4 to 0.7
+	 * 											 for input G/R of -0.7f to -0.9f : 0.1 to 0.4 
 	 */
-	public GreenImageProcesser(float redIndex, float greenIndex, float blueIndex, double upperBound) {
-		this.redIndex = redIndex;
-		this.greenIndex = greenIndex;
-		this.blueIndex = blueIndex;
-		this.upperBound = upperBound;
+	public GreenImageProcesser(float redIndex,float greenIndex,float blueIndex,double upperBound)
+	{
+		this.redIndex=redIndex;
+		this.greenIndex=greenIndex;
+		this.blueIndex=blueIndex;
+		this.upperBound=upperBound;
 	}
-
 	public float getRedIndex() {
 		return redIndex;
 	}
-
 	public void setRedIndex(float redIndex) {
 		this.redIndex = redIndex;
 	}
-
 	public float getBlueIndex() {
 		return blueIndex;
 	}
-
 	public void setBlueIndex(float blueIndex) {
 		this.blueIndex = blueIndex;
 	}
-
 	public float getGreenIndex() {
 		return greenIndex;
 	}
-
 	public void setGreenIndex(float greenIndex) {
 		this.greenIndex = greenIndex;
 	}
-
 	public double getUpperBound() {
 		return upperBound;
 	}
-
 	public void setUpperBound(double upperBound) {
 		this.upperBound = upperBound;
 	}
-
 	/**
-	 * The process method makes OUTSTANDING green color pixel into a 1 and
-	 * everything else into a 0
+	 * The process method makes OUTSTANDING green color pixel into 
+	 * a 1 and everything else into a 0
 	 * 
-	 * @param image:
-	 *            input BufferedImage that needed to be processed
+	 * @param image: input BufferedImage that needed to be processed
 	 * @return a float 2D array that contains only 1 and 0
-	 */
-	public float[][] process(BufferedImage image) {
-		Color[][] asColors = new Color[image.getWidth()][image.getHeight()];
-		for (int y = 0; y < asColors[0].length; y++) {
-			for (int x = 0; x < asColors.length; x++) {
-				asColors[x][y] = new Color(image.getRGB(x, y));
-			}
-		}
-		float[][] luminance = ImageProcessor.luminance(asColors, redIndex, greenIndex, blueIndex); // phone(-.3,1,-.3)
-		ImageProcessor.normalize(luminance);
-
-		float[][] contrastedData = new float[luminance.length][luminance[0].length];
-
-		for (int y = 0; y < luminance[0].length; y++) {
-			for (int x = 0; x < luminance.length; x++) {
-				if (luminance[x][y] > upperBound) {
-					contrastedData[x][y] = 1;
-				} else {
-					contrastedData[x][y] = 0;
-				}
-			}
-		}
-
-		float[][] modifiedData = new float[luminance.length][luminance[0].length];
-		int validMinimum = 5;
-		for (int y = 2; y < luminance[0].length - 2; y++) {
-			for (int x = 2; x < luminance.length - 2; x++) {
-				int counter = 0;
-				for (int i = 0; i < 3; i++) {
-					for (int j = 0; j < 3; j++)
-						counter += contrastedData[x - 1 + i][y - 1 + i];
-					if (counter > validMinimum) {
-						modifiedData[x][y] = 1;
-					} else {
-						modifiedData[x][y] = 0;
-					}
-				}
-			}
-		}
-
-		return modifiedData;
+	 * */
+	public float[][] process(BufferedImage image)
+	{
+	  Color[][] asColors=new Color[image.getWidth()][image.getHeight()];
+	  for (int y=0; y<asColors[0].length; y++) {
+	   for (int x=0; x<asColors.length; x++) {
+	    asColors[x][y]=new Color(image.getRGB(x, y));
+	   }
+	  }
+	  float[][] luminance=ImageProcessor.luminance(asColors, redIndex, greenIndex, blueIndex); //phone(-.3,1,-.3)
+	  ImageProcessor.normalize(luminance);
+	  
+	  float [][] contrastedData=new float[luminance.length][luminance[0].length];
+	  
+	  
+	  for(int y=0;y<luminance[0].length;y++)
+	  {
+		  for(int x=0; x<luminance.length;x++)
+		  {
+			  if (luminance[x][y]>upperBound)
+			  {
+				  contrastedData[x][y]=1;
+			  }
+			  else
+			  {
+				  contrastedData[x][y]=0;
+			  }	  
+		  }
+	  }
+	
+	  float [][]modifiedData=new float [luminance.length][luminance[0].length];
+	  int validMinimum=5;
+	  for(int y=2;y<luminance[0].length-2;y++)
+	  {
+		  for(int x=2; x<luminance.length-2;x++)
+		  {
+			  int counter=0;
+			  for(int i=0;i<3;i++)
+			  {
+				  for(int j=0;j<3;j++)
+					  counter+=contrastedData[x-1+i][y-1+i];
+				  if(counter>validMinimum)
+				  {
+					  modifiedData[x][y]=1;
+				  }
+				  else
+				  {
+					  modifiedData[x][y]=0;
+				  }
+			  }
+		  }
+	  }
+	  
+	  return modifiedData;
 	}
 
 }
