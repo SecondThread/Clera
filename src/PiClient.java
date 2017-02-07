f1d60d912e0b4e7485b87ec9c2cf2621df98975a
Alex <pooky.rusnak@gmail.com>
2017-02-06 19:50:38 -0600

    implemented 2 cameras

--- src/PiClient.java
+++ src/PiClient.java
@@ -18,7 +18,7 @@ import robot.Window;
 
 public class PiClient {
 
-	private static Webcam webcam;
+	private static Webcam pegWebcam, shooterWebcam;
 	private static ArrayList<Point> foundPoints;
 	private static Point shooterPoint;
 	private static double distance;
@@ -41,70 +41,89 @@ public class PiClient {
 			}
 		});
 	}
-	
+
 	public static void init() {
-		webcam=Webcam.getDefault();
-		webcam.setViewSize(new Dimension(320, 240));
-		webcam.open();
+		pegWebcam = Webcam.getWebcams().get(0);
+		System.out.println("Peg webcam: " + pegWebcam.getName());
+		pegWebcam.setViewSize(new Dimension(320, 240));
+		pegWebcam.open();
+
+		shooterWebcam = Webcam.getWebcams().get(1);
+		System.out.println("Shooter webcam: " + shooterWebcam.getName());
+		shooterWebcam.setViewSize(new Dimension(320, 240));
+		shooterWebcam.open();
+
 	}
 
 	public static float getDegreesToTurn(int runCounter) {
-		float[][] luminance=null;
-		ArrayList<Point> bestPoints=null;
-		System.out.println("start: "+System.currentTimeMillis());
-		BufferedImage image=getImageFromWebcam(webcam);
-		Color[][] asColors=new Color[image.getWidth()][image.getHeight()];
+		float[][] luminance = null;
+		ArrayList<Point> bestPoints = null;
+		System.out.println("start: " + System.currentTimeMillis());
+		BufferedImage image = getImageFromWebcam(pegWebcam);
+		Color[][] asColors = new Color[image.getWidth()][image.getHeight()];
 
-		for (int x=0; x<asColors.length; x++) {
-			for (int y=0; y<asColors[x].length; y++) {
-				asColors[x][y]=new Color(image.getRGB(x, y));
+		for (int x = 0; x < asColors.length; x++) {
+			for (int y = 0; y < asColors[x].length; y++) {
+				asColors[x][y] = new Color(image.getRGB(x, y));
 			}
 		}
-		luminance=ImageProcessor.luminance(asColors, -.6f, 1.0f, -.2f);
+		luminance = ImageProcessor.luminance(asColors, -.6f, 1.0f, -.6f);
 		ImageProcessor.normalize(luminance);
 		ImageProcessor.applyExponentialCurve(luminance, 3);
 
-		bestPoints=new ArrayList<Point>();
-		bestPoints=runThreads(luminance);
+		bestPoints = new ArrayList<Point>();
+		bestPoints = runThreads(luminance);
 		Window.displayPixels(luminance, "picture");
-		//null if points are wrong
-		if (bestPoints==null) {
-			if (runCounter>=3) {
-				return 0;
-			}
-			//try it again if we got things wrong
-			return getDegreesToTurn(runCounter+1);
-		}
-		System.out.println("done: "+System.currentTimeMillis());
-		Window.saveImage(luminance, bestPoints, "result.png");
-		foundPoints = bestPoints;
-		Point peg=PegVisionUtils.findPeg(bestPoints);
-		//length of dumbo, 50, width of image, best points
-		distance=PegVisionUtils.calcDistance(bestPoints);
-		return TurnAngle.getTurnAngle(peg);
-	}
-	
-	public static float getShooterDegreesToTurn(int runCounter, BufferedImage shooterImage) {
-		System.out.println("start: " + System.currentTimeMillis());
-		Point shooter = FindOnePtTop.findTopPoint(shooterImage);
-		shooterPoint = shooter;
 		// null if points are wrong
-		if (shooter.equals(new Point(0, 0))) {
+		if (bestPoints == null) {
 			if (runCounter >= 3) {
 				return 0;
 			}
 			// try it again if we got things wrong
-			return getShooterDegreesToTurn(runCounter + 1, shooterImage);
+			return getDegreesToTurn(runCounter + 1);
 		}
 		System.out.println("done: " + System.currentTimeMillis());
-		return TurnAngle.getTurnAngle(shooter);
+		Window.saveImage(luminance, bestPoints, "result.png");
+		foundPoints = bestPoints;
+		Point peg = PegVisionUtils.findPeg(bestPoints);
+		// length of dumbo, 50, width of image, best points
+		distance = PegVisionUtils.calcDistance(bestPoints);
+		return TurnAngle.getTurnAngle(peg);
+	}
+
+	public static float getShooterDegreesToTurn(int runCounter) {
+		float[][] luminance = null;
+		ArrayList<Point> bestPoints = null;
+		System.out.println("start: " + System.currentTimeMillis());
+		BufferedImage image = getImageFromWebcam(shooterWebcam);
+		Color[][] asColors = new Color[image.getWidth()][image.getHeight()];
+
+		for (int x = 0; x < asColors.length; x++) {
+			for (int y = 0; y < asColors[x].length; y++) {
+				asColors[x][y] = new Color(image.getRGB(x, y));
+			}
+		}
+		Point topPoint = FindOnePtTop.findTopPoint(image);
+
+		Window.displayPixels(asColors, "shooter picture");
+		// null if points are wrong
+		if (topPoint.equals(new Point())) {
+			if (runCounter >= 3) {
+				return 0;
+			}
+			// try it again if we got things wrong
+			return getShooterDegreesToTurn(runCounter + 1);
+		}
+		System.out.println("done: " + System.currentTimeMillis());
+		return TurnAngle.getTurnAngle(topPoint);
+
 	}
 
 	private static ArrayList<Point> runThreads(float[][] image) {
-		ImageSearchingThread topLeft=new ImageSearchingThread(topLeftTemplate, image);
-		ImageSearchingThread topRight=new ImageSearchingThread(topRightTemplate, image);
-		ImageSearchingThread bottomLeft=new ImageSearchingThread(bottomLeftTemplate, image);
-		ImageSearchingThread bottomRight=new ImageSearchingThread(bottomRightTemplate, image);
+		ImageSearchingThread topLeft = new ImageSearchingThread(topLeftTemplate, image);
+		ImageSearchingThread topRight = new ImageSearchingThread(topRightTemplate, image);
+		ImageSearchingThread bottomLeft = new ImageSearchingThread(bottomLeftTemplate, image);
+		ImageSearchingThread bottomRight = new ImageSearchingThread(bottomRightTemplate, image);
 
 		new Thread(topRight).start();
 		new Thread(bottomLeft).start();
@@ -114,7 +133,7 @@ public class PiClient {
 		// bottomRight.run();
 		topLeft.run();
 
-		while (!topLeft.isDone()||!topRight.isDone()||!bottomLeft.isDone()||!bottomRight.isDone()) {
+		while (!topLeft.isDone() || !topRight.isDone() || !bottomLeft.isDone() || !bottomRight.isDone()) {
 			try {
 				Thread.sleep(5);
 			} catch (InterruptedException e) {
@@ -122,14 +141,14 @@ public class PiClient {
 			}
 		}
 
-		ArrayList<Point> toReturn=new ArrayList<Point>();
+		ArrayList<Point> toReturn = new ArrayList<Point>();
 		toReturn.addAll(topLeft.bestPoints);
 		toReturn.addAll(topRight.bestPoints);
 		toReturn.addAll(bottomLeft.bestPoints);
 		toReturn.addAll(bottomRight.bestPoints);
 
-		//null if the points are wrong
-		toReturn=PegVisionUtils.generateNewPoints(toReturn);
+		// null if the points are wrong
+		toReturn = PegVisionUtils.generateNewPoints(toReturn);
 		System.out.println("!!!!!!!!!!!!!!generated new points: " + toReturn);
 
 		return toReturn;
@@ -140,16 +159,16 @@ public class PiClient {
 		return webcam.getImage();
 	}
 
-	private static float[][] topLeftTemplate= {{0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0,},
-			{0, 0, 0, 1, 1, 1,}, {0, 0, 0, 1, 1, 1,}, {0, 0, 0, 1, 1, 1,},};
+	private static float[][] topLeftTemplate = { { 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0, },
+			{ 0, 0, 0, 1, 1, 1, }, { 0, 0, 0, 1, 1, 1, }, { 0, 0, 0, 1, 1, 1, }, };
 
-	private static float[][] topRightTemplate= {{0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0},
-			{1, 1, 1, 0, 0, 0}, {1, 1, 1, 0, 0, 0}, {1, 1, 1, 0, 0, 0},};
+	private static float[][] topRightTemplate = { { 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0 },
+			{ 1, 1, 1, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 }, };
 
-	private static float[][] bottomLeftTemplate= {{0, 0, 0, 1, 1, 1,}, {0, 0, 0, 1, 1, 1,}, {0, 0, 0, 1, 1, 1,},
-			{0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0,}, {0, 0, 0, 0, 0, 0,},};
+	private static float[][] bottomLeftTemplate = { { 0, 0, 0, 1, 1, 1, }, { 0, 0, 0, 1, 1, 1, }, { 0, 0, 0, 1, 1, 1, },
+			{ 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0, }, { 0, 0, 0, 0, 0, 0, }, };
 
-	private static float[][] bottomRightTemplate= {{1, 1, 1, 0, 0, 0}, {1, 1, 1, 0, 0, 0}, {1, 1, 1, 0, 0, 0},
-			{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0},};
+	private static float[][] bottomRightTemplate = { { 1, 1, 1, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, };
 
 }
