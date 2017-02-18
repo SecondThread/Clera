0a49e9add724b478978b53c88ae44fbc496b2a97
BeastRemus <beastremus@gmail.com>
2017-02-17 19:22:25 -0600

    Fixed compile error

--- src/mainClasses/HighGoal.java
+++ src/mainClasses/HighGoal.java
@@ -50,7 +50,7 @@ public class HighGoal {
 					System.out.println("Sending picture");
 					image=vision.getImage();
 					image=ImageProcessor.scaleImage(image, 134);
-					table.putBoolean("HighGoalVisionSucceeded", value);
+					table.putBoolean("HighGoalVisionSucceeded", vision.getVisionSucceeded());
 					table.putString("PictureHighGoal", ConvertToString.convertToString(image));
 					table.putBoolean("NeedPictureHighGoal", false);
 				}
