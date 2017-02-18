cf227fbc8c9bcd080b26066051988dd5df53c5a1
DaSpook <gummaditanay@gmail.com>
2017-02-17 20:30:05 -0600

    Added Event Listener for DriverStationClient

--- src/mainClasses/DriverStationClient.java
+++ src/mainClasses/DriverStationClient.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JButton;
@@ -35,6 +37,28 @@ public class DriverStationClient {
 		
 		
 		frame = new JFrame();
+		frame.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_S) {
+					showHighGoalVision = !showHighGoalVision;
+				}
+				
+			}
+		});
 		mainPanel = new JPanel();
 		outerPanel=new JPanel();
 		frame.add(outerPanel);
